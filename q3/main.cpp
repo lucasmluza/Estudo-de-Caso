d6f2cb5ec069e1cd5d013a299dc142d5d1ccae6e
Lucas Matana Luza <lucas.luza@sc.senai.br>
2023-02-28 13:56:37 -0300

    Add prototipo da main

---
+++ q3/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("subtraction 3.5 - 2.5: %d\r\n", subtraction(3.5, 2.5));
+    printf("multiplication 3.5 * 2.5: %d\r\n", multiplication(3.5, 2.5));
+    printf("sum 3.5 + 2.5: %d\r\n", sum(3.5, 2.5));
+    printf("division 3.5 / 2.5: %d\r\n", division(3.5, 2.5));
+
+    return 0;
+}
